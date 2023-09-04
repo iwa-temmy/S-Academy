75032b527d9141c9d485b7e40f8ee1c9e20053e6
olawalecodes <iwaloyeo@gmail.com>
2023-09-04 01:08:28 +0100

    wip

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Button } from "@mui/material";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-[red]">Testing</h1>
+      <Button>Hello</Button>
+    </div>
+  );
+}
+
+export default App;
