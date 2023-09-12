92e5964216955ffc558be53b8709b673e7f7336d
olawalecodes <iwaloyeo@gmail.com>
2023-09-12 14:53:56 +0100

    chores: admin reset password page

--- src/views/auth/AdminResetPassword.jsx
+++ src/views/auth/AdminResetPassword.jsx
@@ -1,30 +1,41 @@
 import AppInput from "../../components/AppInput";
-import AppLoadingButton from "../../components/AppLoadingButton";
 import Authheader from "./components/Authheader";
-import { useTheme } from "@mui/styles";
+import AppLoadingButton from "../../components/AppLoadingButton";
+import { useTheme } from "@mui/material";
+
 const AdminResetPassword = () => {
   const theme = useTheme();
   return (
     <div className="auth-container">
       <div className="flex flex-col items-center h-[90vh] lg:h-[73vh] w-11/12 lg:w-2/5 m-auto mt-6 relative">
-        <Authheader text="Reset password" />
+        <Authheader text="Reset Password" />
         <form className="w-full pt-14 px-16 lg:px-20">
           <AppInput
-            name="email"
-            label="Email"
-            height="large"
-            type="email"
-            fullWidth
-          />
-          <AppInput
-            name="password"
-            label="Password"
+            name="old_password"
+            label="Old Password"
             height="large"
             type="password"
             fullWidth
+            sx={{ my: 1 }}
+          />
+          <AppInput
+            name="new_password"
+            label="New Password"
+            height="large"
+            type="password"
+            fullWidth
+            sx={{ my: 1 }}
+          />
+          <AppInput
+            name="confirm_password"
+            label="Confirm Password"
+            height="large"
+            type="password"
+            fullWidth
+            sx={{ my: 1 }}
           />
           <AppLoadingButton
-            text="Sign in"
+            text="Reset"
             sx={{
               backgroundColor: theme.palette.primary[30],
               width: "100%",
