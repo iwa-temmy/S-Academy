a28563c1d96714645acc2fbc603a72bb7b4bfd0b
bamianike@gmail.com <bamianike@gmail.com>
2023-09-14 11:26:54 +0100

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,14 @@ import theme from "./assets/theme/theme";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/RootRouter";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <RouterProvider router={router} />
+      <GoogleOAuthProvider clientId="671348139606-906f7lcl8vk6l26hivc1ka0hk2teuvb1.apps.googleusercontent.com">
+        <RouterProvider router={router}></RouterProvider>
+      </GoogleOAuthProvider>
     </ThemeProvider>
   </React.StrictMode>
 );
