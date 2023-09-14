361118591862ec8d6e6d58bef7aadca77c06750c
olawalecodes <iwaloyeo@gmail.com>
2023-09-14 08:12:42 +0100

    chores: clean up

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "./assets/theme/theme";
 import "./index.css";
